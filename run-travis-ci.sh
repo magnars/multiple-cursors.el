dc0aa994594fd24858da1c51f6cd4b37db5cf533
Steve Purcell <steve@sanityinc.com>
2012-11-27 16:24:44 +0000

    Simplify travis run script, and make it work when ECUKES_EMACS has a space in the path
    Yes, on OS X, one might have:
    
    ECUKES_EMACS='/Applications/Emacs 23.app/Contents/MacOS/Emacs'

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
 cd "$(dirname "$0")"
 
-set_default () {
-  eval "
-if [ -z \$$1 ]; then
-  $1=$2
-fi
-"
-}
-
-set_default ECUKES_EMACS "$(which emacs)"
+ECUKES_EMACS=${ECUKES_EMACS:-$(which emacs)}
 
 echo "*** Emacs version ***"
-echo "ECUKES_EMACS =" $(which $ECUKES_EMACS)
-$ECUKES_EMACS --version
+echo "ECUKES_EMACS = $ECUKES_EMACS"
+"$ECUKES_EMACS" --version
 echo
 
 exec ./util/ecukes/ecukes --graphical
