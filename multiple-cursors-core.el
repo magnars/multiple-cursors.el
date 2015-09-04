5a6a05cdef42990bdf4c5616126b5b7067464070
Martin Yrjölä <martin.yrjola@gmail.com>
2015-09-05 00:29:05 +0300

    One more cursor-specific-variable

--- multiple-cursors-core.el
+++ multiple-cursors-core.el
@@ -142,6 +142,7 @@ highlights the entire width of the window."
                                   autopair-action
                                   autopair-wrap-action
                                   er/history
+                                  temporary-goal-column
                                   evil-state
                                   evil-previous-state
                                   evil-next-state
