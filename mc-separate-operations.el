6a62e60bf2920b0790ce27dfce8afd7c9f079f80
jtamagnan <jtamagnan@gmail.com>
2016-04-27 22:33:15 -0400

    Fix issue #131 (#247)
    * Fix issue #131
    
    This issue would occur because the function was recieving a raw prefix
    arg instead of a number. This commit fixes that.
    
    * Fix issue with previous commit, add test cases

--- mc-separate-operations.el
+++ mc-separate-operations.el
@@ -33,7 +33,8 @@
 (defun mc/insert-numbers (arg)
   "Insert increasing numbers for each cursor, starting at 0 or ARG."
   (interactive "P")
-  (setq mc--insert-numbers-number (or arg 0))
+  (setq mc--insert-numbers-number (or (and arg (prefix-numeric-value arg))
+                                      0))
   (mc/for-each-cursor-ordered
    (mc/execute-command-for-fake-cursor 'mc--insert-number-and-increase cursor)))
 
@@ -58,7 +59,8 @@
   "Insert increasing letters for each cursor, starting at 0 or ARG.
      Where letter[0]=a letter[2]=c letter[26]=aa"
   (interactive "P")
-  (setq mc--insert-letters-number (or arg 0))
+  (setq mc--insert-letters-number (or (and arg (prefix-numeric-value arg))
+                                      0))
   (mc/for-each-cursor-ordered
    (mc/execute-command-for-fake-cursor 'mc--insert-letter-and-increase cursor)))
 
